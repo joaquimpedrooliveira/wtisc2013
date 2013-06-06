d512106de36785a2acfc50680f19fa28fe06fec9
joaquimpedrooliveira <joaquimpedrooliveira@gmail.com>
2013-06-05 22:20:00 -0300

    Revendo a ordem dos exemplos.

--- src/br/ufc/quixada/wtisc2013/Exemplo05.java
+++ src/br/ufc/quixada/wtisc2013/Exemplo05.java
@@ -2,24 +2,34 @@ package br.ufc.quixada.wtisc2013;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Exemplo05 {
 
-	public static void main(String a[]) {
-		String texto = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-		List<String> variasLinhas = new ArrayList<String>();
+	private static void criaObjetosComConstrutor(List<Integer> lista) {
+		for (int i=0; i < 2000000; i++) {
+			lista.add(new Integer(1));
+		}
+	}
+	
+	private static void criaObjetosComValueOf(List<Integer> lista) {
+		for (int i=0; i < 2000000; i++) {
+			lista.add(Integer.valueOf(1));
+		}
+	}
+	
+	public static void main(String[] args) {
+		List<Integer> lista = new ArrayList<Integer>();
 		System.out.println("Antes:" + memoriaUtilizada() + " bytes");
 		long tempoInicial = System.currentTimeMillis();
-		for (int i=0;  i<2000000; i++) {
-			variasLinhas.add(new String("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."));
-			//variasLinhas.add("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
-		}
+		criaObjetosComConstrutor(lista);
+		//criaObjetosComValueOf(lista);
 		long tempoFinal = System.currentTimeMillis();
 		System.out.println("Depois:" + memoriaUtilizada() + " bytes");
-		System.out.println("Tempo de execução: " + (tempoFinal - tempoInicial) + " segundos.");
- 	}
+		System.out.println("Tempo de execução: " + (tempoFinal - tempoInicial) + " milissegundos.");
 
+	}
+	
 	private static long memoriaUtilizada() {
 		return (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory());
 	}
-
 }
